5d4e35e695f95eced24bbef7dd05a8b635cf63a6
Gaurvi-Gupta <136971065+Gaurvi-Gupta@users.noreply.github.com>
2025-04-10 04:17:18 +0000

    Conversion of Datatypes

---
+++ .devcontainer/basics/Conversion_Operation.js
@@ -0,0 +1,23 @@
+let score = "34abd";
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+//"33abc" => NaN
+//"gaurvi" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "gaurvi";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false;
+// "gaurvi" => true;
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
