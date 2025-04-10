2a06b214db6c06ec512832cf55a1bb357ba1390b
Gaurvi-Gupta <136971065+Gaurvi-Gupta@users.noreply.github.com>
2025-04-10 06:00:13 +0000

    Operations

--- .devcontainer/basics/Conversion_Operation.js
+++ .devcontainer/basics/Conversion_Operation.js
@@ -19,5 +19,40 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+// ********************************Operations***************************************8
+
+let value = 3;
+let negvalue = -value;
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " gaurvi"
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((2 + 3) * 5 / 1);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
