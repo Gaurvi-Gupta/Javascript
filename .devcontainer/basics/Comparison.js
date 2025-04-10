3cb30cfe6db21a8d62474ceb242ce44145e271c3
Gaurvi-Gupta <136971065+Gaurvi-Gupta@users.noreply.github.com>
2025-04-10 07:26:30 +0000

    Comparison

---
+++ .devcontainer/basics/Comparison.js
@@ -0,0 +1,13 @@
+// console.log("2" > 1);
+
+// console.log(null > 0); //false
+// console.log(null == 0); // false
+// console.log(null >= 0); // true // Equality assignment operation and comparison operator works differently
+// comparison operator converts null to 0 
+
+// console.log(undefined == 0); // false
+// console.log(undefined > 0); // false
+// console.log(undefined < 0); // false
+
+console.log("2" === 2); // false;
+// === is a strict check operator it checks value as well as datatype
