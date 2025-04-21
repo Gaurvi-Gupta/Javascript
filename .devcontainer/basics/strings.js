39a1116f0a4ab84006954487d53dde1b7dc16087
Gaurvi-Gupta <136971065+Gaurvi-Gupta@users.noreply.github.com>
2025-04-21 13:37:34 +0000

    Strings

---
+++ .devcontainer/basics/strings.js
@@ -0,0 +1,14 @@
+const name = "gaurvi";
+const repoCount = 9;
+
+// console.log(`My name is ${name} and my repoCount is ${repoCount}`);
+
+const gameName = new String('gaurviii');
+
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('g'));
