439e2be2280d3450c7d7ffab7828c072e6bed7a6
Gaurvi-Gupta <136971065+Gaurvi-Gupta@users.noreply.github.com>
2025-04-22 06:02:51 +0000

    Learn some more functions of string

--- .devcontainer/basics/strings.js
+++ .devcontainer/basics/strings.js
@@ -10,5 +10,21 @@ const gameName = new String('gaurviii');
 // console.log(gameName.length)
 // console.log(gameName.toUpperCase());
 
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('g'));
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('g'));
+
+const newstring = gameName.substring(0,4);
+// console.log(newstring);
+
+const anotherstring = gameName.slice(-5,4);
+// console.log(anotherstring);
+
+const newstringone = "     gaurvi       ";
+// console.log(newstringone);
+// console.log(newstringone.trim());
+
+const url = "https://gaurvi-gupta-gg.com";
+// console.log(url.replace('.', '-'));
+// console.log(url.includes('gaurvi'));
+
+console.log(url.split('-'));
